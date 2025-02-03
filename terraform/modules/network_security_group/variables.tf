b1d38e601f4c3a1eb10c729dd7d2a60e1da99812
Noorr99 <135596143+Noorr99@users.noreply.github.com>
2025-02-03 12:16:15 +0300

    add var to nsg module

--- terraform/modules/network_security_group/variables.tf
+++ terraform/modules/network_security_group/variables.tf
@@ -28,3 +28,25 @@ variable "log_analytics_workspace_id" {
   description = "Specifies the log analytics workspace resource id"
   type        = string
 }
+
+variable "security_rules" {
+  description = "(Optional) Specifies the security rules of the network security group"
+  type = list(object({
+    name                                       = string
+    priority                                   = number
+    direction                                  = string
+    access                                     = string
+    protocol                                   = string
+    source_port_range                          = optional(string)
+    source_port_ranges                         = optional(list(string))
+    destination_port_range                     = optional(string)
+    destination_port_ranges                    = optional(list(string))
+    source_address_prefix                      = optional(string)
+    source_address_prefixes                    = optional(list(string))
+    destination_address_prefix                 = optional(string)
+    destination_address_prefixes               = optional(list(string))
+    source_application_security_group_ids      = optional(list(string))
+    destination_application_security_group_ids = optional(list(string))
+  }))
+  default = []
+}
